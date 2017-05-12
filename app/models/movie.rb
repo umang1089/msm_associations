fd66f686a59beb6dbab8eabad00b944e8a490fd3
Umang Shukla <Umang1089@gmail.com>
2017-05-12 12:50:58 -0500

    100%
    Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,7 +10,7 @@ validates :title, :presence => true, :uniqueness => { :scope => :year }
 validates :year, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1870, :less_than_or_equal_to => 2050}
 
 # - duration: must be integer between 0 and 2764800
-validates :duration, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800}
+validates :duration, :numericality => { :only_integer => true, :allow_blank => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800}
 
 # - description: no rules
 # - image_url: no rules
@@ -18,6 +18,7 @@ validates :duration, :numericality => { :only_integer => true, :greater_than_or_
 belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
 
 has_many(:characters, :class_name => "Character", :foreign_key => "movie_id")
-has_many(:actors, :class_name => "Actor", :foreign_key => "movie_id")
+
+has_many(:actors, :through => :characters)
 
 end
